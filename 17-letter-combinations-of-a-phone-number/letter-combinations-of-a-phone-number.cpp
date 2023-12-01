2e3813986c1c8eaa74d947a0000bfd0d8580ff9f
SD691 <110734465+SD691@users.noreply.github.com>
2023-12-01 18:58:34 +0530

    Time: 2 ms (42.05%) | Memory: 8.7 MB (5.00%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    vector<string> ans;
-    map<int,string> mp;
-    void helper(string &digits, int start, string&temp){
+
+    void helper(string &digits, int start, string &temp, vector<string> &ans, map<int,string> mp){
         if(start == digits.size()){
             ans.push_back(temp);
+            return;
         }
 
         for(auto it: mp[digits[start]-'0']){
             temp.push_back(it);
-            helper(digits,start+1,temp);
+            helper(digits,start+1,temp,ans,mp);
             temp.pop_back();
         }
     }
-
     vector<string> letterCombinations(string digits) {
-       
-        if(digits==""){
-            return ans;
+        if(digits.size()==0){
+            return {};
         }
+        map<int,string> mp;
 
         mp[2] = "abc";
         mp[3] = "def";
@@ -28,8 +27,10 @@ public:
         mp[7] = "pqrs";
         mp[8] = "tuv";
         mp[9] = "wxyz";
+
         string temp;
-        helper(digits,0,temp);
+        vector<string> ans;
+        helper(digits,0,temp,ans,mp);
         return ans;
     }
 };
