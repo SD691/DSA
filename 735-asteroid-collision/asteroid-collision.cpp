4dff1a70fd0281668702b633f70fd5a659316df1
SD691 <110734465+SD691@users.noreply.github.com>
2023-12-09 02:39:46 +0530

    Time: 7 ms (89.73%) | Memory: 18.1 MB (67.56%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-         std::stack<int> st;
+        stack<int> st;
         
-        for (int i = 0; i < asteroids.size(); ++i) {
-            if (st.empty() || asteroids[i] > 0 || st.top() < 0) {
+
+        for (int i = 0; i < asteroids.size(); i++) {
+            if (st.empty() || st.top() < 0 || asteroids[i] > -1) {
                 st.push(asteroids[i]);
             } else {
                 while (!st.empty() && st.top() > 0 && abs(asteroids[i]) > abs(st.top())) {
@@ -14,7 +15,7 @@ public:
                 if (st.empty() || st.top() < 0) {
                     st.push(asteroids[i]);
                 } else if (abs(asteroids[i]) == abs(st.top())) {
-                    st.pop(); // Destroy both asteroids
+                    st.pop();
                 }
             }
         }
