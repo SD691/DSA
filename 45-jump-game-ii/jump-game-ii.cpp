60d047dc0c1b3d5d73bf594b016f9d8f9a4932fb
SD691 <110734465+SD691@users.noreply.github.com>
2023-12-23 19:23:19 +0530

    Time: 8 ms (90.11%) | Memory: 16.8 MB (98.13%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n = nums.size();
-
-        int jumps = 0, currEnd = 0, currFarthest = 0;
-
-        for(int i = 0; i<n-1; i++){
-
-            currFarthest = max(currFarthest, i+nums[i]);
-
-            if(i==currEnd){
-                jumps++;
-                currEnd = currFarthest;
+        if(nums.size()==1) return 0;
+        int count = 0;
+        int curr_dis = nums[0]+0;
+        int next_dis = 0;
+        int prev_dis = 0;
+        int pt = 0;
+        int n = nums.size()-1;
+        for(int i=1; i<nums.size(); i++){
+            if(curr_dis>=n){
+                count++;
+                break;
             }
-        }
-        return jumps;
-    }
+            next_dis = nums[i]+i;
+            if(curr_dis<next_dis){
+                if(pt==0){
+                    prev_dis = curr_dis;
+                    curr_dis = next_dis;
+                    count++;
+                    pt++;
+                }
+                else if(i<=prev_dis){
+                    curr_dis = next_dis;
+                }
+                else{
+                    prev_dis = curr_dis;
+                    curr_dis = next_dis;
+                    count++;
+                }
+               
+            }
 
-    /*the intution behing this solution is
-    travel the distance equivalent to the value of the i+nums[i]
-    now when we reach that distance 
-    then we are going to find which element can take us to the futher element
-    this we are going to figure out from currfarthest.
-    thus eventually we will reach the end adn will return the jumos*/
+        }
+        return count;
+    }
 };
