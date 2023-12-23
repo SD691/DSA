810ad65977c0dbf61441d413305a4b20ed8cdc5f
SD691 <110734465+SD691@users.noreply.github.com>
2023-12-23 19:34:11 +0530

    Time: 13 ms (54.34%) | Memory: 16.9 MB (54.35%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        if(nums.size()==1) return 0;
-        int count = 0;
-        int curr_dis = nums[0]+0;
-        int next_dis = 0;
-        int prev_dis = 0;
-        int pt = 0;
-        int n = nums.size()-1;
-        for(int i=1; i<nums.size(); i++){
-            if(curr_dis>=n){
-                count++;
-                break;
+        int reach=0, count=0, last=0;  // reach: maximum reachable index from current position
+    // count: number of jumps made so far
+    // last: rightmost index that has been reached so far
+        for(int i=0;i<nums.size()-1;i++){  // loop through the array excluding the last element
+            reach = max(reach, i+nums[i]);  // update reach to the maximum between reach and i+nums[i]
+            if(i==last){  // if i has reached the last index that can be reached with the current number of jumps
+                last = reach;  // update last to the new maximum reachable index
+                count++;  // increment the number of jumps made so far
             }
-            next_dis = nums[i]+i;
-            if(curr_dis<next_dis){
-                if(pt==0){
-                    prev_dis = curr_dis;
-                    curr_dis = next_dis;
-                    count++;
-                    pt++;
-                }
-                else if(i<=prev_dis){
-                    curr_dis = next_dis;
-                }
-                else{
-                    prev_dis = curr_dis;
-                    curr_dis = next_dis;
-                    count++;
-                }
-               
-            }
-
         }
-        return count;
+        return count;  //
     }
 };
