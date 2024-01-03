ad463619bdf0caa30d54fff71d29dfc35b1b39d1
SD691 <110734465+SD691@users.noreply.github.com>
2024-01-03 20:42:34 +0530

    Time: 75 ms (96.28%) | Memory: 29.3 MB (24.35%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        vector<int> no_of_one;
-        for(int i=0; i<bank.size(); i++){
-            int count = 0;
-            for(int j=0; j<bank[i].size(); j++){
-                if(bank[i][j]=='1'){
-                    count++;
+        int prev_count = 0;
+        int ans = 0;
+
+        for(auto it: bank){
+            int current_count = 0;
+
+            for(char c: it){
+                if(c == '1'){
+                    current_count++;
                 }
             }
-            if(count>0){
-                no_of_one.push_back(count);
+            if(current_count>0){
+                ans += prev_count*current_count;
+                prev_count = current_count;
             }
-        }
-        int ans = 0;
-        if(no_of_one.size()<=1) return 0;
-        for(int i=1; i<no_of_one.size(); i++){
-            ans = ans + (no_of_one[i]*no_of_one[i-1]);
         }
         return ans;
     }
