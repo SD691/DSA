7df9d10b1d9c553bcf3c85e470b1742a94bf0b95
SD691 <110734465+SD691@users.noreply.github.com>
2024-01-04 19:06:52 +0530

    Time: 14 ms (89.21%) | Memory: 13.5 MB (92.07%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int V = numCourses; // Number of courses
-        vector<int> adj[V];
-        vector<int> ind(V, 0); 
-        
+        vector<int> adj[numCourses];
+        vector<int> ind(numCourses,0);
 
-        for(auto it: prerequisites){
-            adj[it[1]].push_back(it[0]);
-            ind[it[0]]++;
+        for(int i=0; i<prerequisites.size(); i++){
+            int a =prerequisites[i][0];
+            int b = prerequisites[i][1];
+            adj[b].push_back(a);
+            ind[a]++;
         }
 
         queue<int> q;
-        for (int i = 0; i < V; i++) {
-            if (ind[i] == 0) {
-                q.push(i);
-            }
+        for(int i=0; i<numCourses; i++){
+            if(ind[i]==0) q.push(i);
         }
 
         vector<int> topo;
-        while (!q.empty()) {
+        while(!q.empty()){
             int node = q.front();
             q.pop();
             topo.push_back(node);
 
-            for (auto it : adj[node]) {
+            for(auto it: adj[node]){
                 ind[it]--;
-                if (ind[it] == 0) {
-                    q.push(it);
-                }
+                if(ind[it]==0) q.push(it);
             }
         }
 
-        if (topo.size() == numCourses) {
-            return true;
-        }
+        if(topo.size()==numCourses) return true;
         return false;
-            
-        
     }
 };
