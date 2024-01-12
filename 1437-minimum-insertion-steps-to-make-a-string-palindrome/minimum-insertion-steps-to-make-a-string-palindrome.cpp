c80079f3b4a6d619e0f29f186811e970a21d8d99
SD691 <110734465+SD691@users.noreply.github.com>
2024-01-12 20:33:58 +0530

    Time: 73 ms (37.88%) | Memory: 29 MB (16.72%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,45 +1,25 @@
 class Solution {
 public:
-
-int lcs(string s1, string s2) {
-    int n = s1.size();
-    int m = s2.size();
-
-    // Create a 2D DP array to store the length of the LCS
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-
-    // Initialize the first row and first column to 0
-    for (int i = 0; i <= n; i++) {
-        dp[i][0] = 0;
-    }
-    for (int i = 0; i <= m; i++) {
-        dp[0][i] = 0;
-    }
-
-    // Fill in the DP array
-    for (int ind1 = 1; ind1 <= n; ind1++) {
-        for (int ind2 = 1; ind2 <= m; ind2++) {
-            if (s1[ind1 - 1] == s2[ind2 - 1])
-                dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
-            else
-                dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
-        }
-    }
-
-    // The value at dp[n][m] contains the length of the LCS
-    return dp[n][m];
-}
-
-// Function to calculate the length of the Longest Palindromic Subsequence
-int longestPalindromeSubsequence(string s) {
-    // Create a reversed copy of the string
-    string t = s;
-    reverse(s.begin(), s.end());
-
-    // Call the LCS function to find the length of the Longest Common Subsequence
-    return lcs(s, t);
-}
     int minInsertions(string s) {
-        return s.size()-longestPalindromeSubsequence(s);
+        string t(s.rbegin(),s.rend());
+        int n = s.size();
+        int m = t.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        for(int i=0; i<=n; i++){
+            dp[i][0]=0;
+            dp[0][i]=0;
+        }
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 0+max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+
+        return s.size()-dp[n][m];
     }
 };
