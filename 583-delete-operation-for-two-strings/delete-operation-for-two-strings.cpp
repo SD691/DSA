d9d38543e038aaaf928621ef2a018c0f4024be51
SD691 <110734465+SD691@users.noreply.github.com>
2024-01-13 15:49:00 +0530

    Time: 21 ms (44.99%) | Memory: 12.5 MB (32.52%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-int longestSubsequence(string &s1, string &s2, int ind1, int ind2, vector<vector<int>> &dp){
-        if(ind1<0 || ind2<0) 
-            return 0;
-
-        if(dp[ind1][ind2]!=-1) 
-            return dp[ind1][ind2];
-
-        if(s1[ind1]==s2[ind2]) 
-            return dp[ind1][ind2] = 1 + longestSubsequence(s1,s2,ind1-1,ind2-1,dp);
-        else
-            return dp[ind1][ind2] = 0 + max(longestSubsequence(s1,s2,ind1-1,ind2,dp),longestSubsequence(s1,s2,ind1,ind2-1,dp));
-        
-    }
-
-   
-
-    int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size();
-        int m = text2.size();
-
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return longestSubsequence(text1,text2,n-1,m-1,dp);
-    }
-
     int minDistance(string word1, string word2) {
-        int ans = longestCommonSubsequence(word1,word2);
         int n = word1.size();
         int m = word2.size();
-        int original = ans;
-        int fin = 0;
-        fin = n-ans;
-        fin = fin + m-ans;
-        return fin;
+
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        for(int i=0; i<=n; i++){
+            dp[i][0] = 0;
+        }
+        for(int i=0; i<=m; i++){
+            dp[0][i] = 0;
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 0 + max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        int ans = dp[n][m];
+        
+        if(n!=m){
+            return (n-ans)+(m-ans);
+        }
+        return 2*(n-ans);
     }
 };
