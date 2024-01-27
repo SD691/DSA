3b35ae077f07ecba8453873d8eb9a4631203f1f0
SD691 <110734465+SD691@users.noreply.github.com>
2024-01-27 17:39:16 +0530

    Time: 54 ms (79.18%) | Memory: 51.4 MB (8.75%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,12 +2,15 @@ class Solution {
 public:
     int helper(int ind, vector<int> &coins, int amount, vector<vector<int>> &dp){
         
-        if(ind==coins.size()) return 1e9;
+        if(ind==0){
+            if(amount%coins[0]==0) return amount/coins[0];
+            else return 1e9;
+        }
         if(amount==0) return 0;
         if(dp[ind][amount]!=-1) return dp[ind][amount];
-        int notTaken = helper(ind+1,coins,amount,dp);
+        int notTaken = helper(ind-1,coins,amount,dp);
         int taken = 1e9;
-        if(amount-coins[ind]>=0){
+        if(coins[ind]<=amount){
             amount= amount-coins[ind];
             taken = 1+helper(ind,coins,amount,dp);
             amount = amount+coins[ind];
@@ -18,7 +21,7 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
-        int val = helper(0,coins,amount,dp);
+        int val = helper(n-1,coins,amount,dp);
         if(val==1e9) return -1;
         return val;
     }
