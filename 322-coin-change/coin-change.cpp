990b26d95a83db3730b5d85f332ffebe923879f7
SD691 <110734465+SD691@users.noreply.github.com>
2024-01-27 17:31:04 +0530

    Time: 68 ms (60.25%) | Memory: 51.9 MB (8.45%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-
-    int minElement(vector<int> &coins, int ind, int target, vector<vector<int>> &dp){
-        if(ind == 0){
-            if(target%coins[0] == 0) return target/coins[0];
-            else return 1e9;
-        }
-
-        if(dp[ind][target]!=-1) return dp[ind][target];
-
-        int notTaken = 0+minElement(coins,ind-1,target,dp);
+    int helper(int ind, vector<int> &coins, int amount, vector<vector<int>> &dp){
+        
+        if(ind==coins.size()) return 1e9;
+        if(amount==0) return 0;
+        if(dp[ind][amount]!=-1) return dp[ind][amount];
+        int notTaken = helper(ind+1,coins,amount,dp);
         int taken = 1e9;
-        if(coins[ind]<=target){
-            taken = 1+minElement(coins,ind,target-coins[ind],dp);
+        if(amount-coins[ind]>=0){
+            amount= amount-coins[ind];
+            taken = 1+helper(ind,coins,amount,dp);
+            amount = amount+coins[ind];
         }
-        return dp[ind][target] = min(notTaken,taken);
+        return dp[ind][amount] = min(taken,notTaken);
+
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans = minElement(coins,n-1,amount,dp);
-        if(ans>=1e9) return -1;
-        return ans;
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+        int val = helper(0,coins,amount,dp);
+        if(val==1e9) return -1;
+        return val;
     }
 };
