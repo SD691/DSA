348f507a8e9a9fc6fcd56c7b9ae17291e5936549
SD691 <110734465+SD691@users.noreply.github.com>
2024-01-28 19:49:48 +0530

    Time: 8 ms (80.16%) | Memory: 20.2 MB (16.48%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-  
+    int helper(int ind, vector<int> &coins, int amount, vector<vector<int>> &dp, int &sum){
+        if(sum==amount){
+            return 1;
+        }
+        if(ind<0) return 0;
+
+        if(dp[ind][sum]!=-1) return dp[ind][sum];
+        int notTaken = helper(ind-1,coins,amount,dp,sum);
+        int taken = 0;
+        if(sum+coins[ind]<=amount){
+            sum = sum+coins[ind];
+            taken = helper(ind,coins,amount,dp,sum);
+            sum = sum-coins[ind];
+        }
+        return dp[ind][sum] = taken+notTaken;
+    }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
-
-        for(int i=0; i<=amount; i++){
-            if(i%coins[0]==0) dp[0][i]=1;
-            
-        }
-        
-
-        for(int i=1; i<n; i++){
-            for(int j=0; j<=amount; j++){
-                int taken = 0;
-                if(coins[i]<=j){
-                    taken = dp[i][j-coins[i]];
-                }
-                int notTaken = dp[i-1][j];
-                dp[i][j] = taken+notTaken;
-            }
-        }
-        return dp[n-1][amount];
-        
+        int sum = 0;
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        return helper(n-1,coins,amount,dp,sum);
     }
 };
