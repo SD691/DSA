6a2f386d0fc62172e6900e13699f9aed69455a6c
SD691 <110734465+SD691@users.noreply.github.com>
2024-02-03 21:17:02 +0530

    Time: 483 ms (21.31%) | Memory: 294.6 MB (5.04%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        vector<int> dp(nums.size(),1);
-        int maxi = 1;
-        for(int i=0; i<nums.size(); i++){
-            for(int prev =0; prev<i; prev++){
-                if(nums[prev]<nums[i]){
-                    dp[i] = max(dp[i],dp[prev]+1);
-                }
-            }
-           maxi = max(dp[i],maxi);
+    int getAns(vector<int> &nums, int ind,int pre_ind, int n, vector<vector<int>> &dp){
+        if(ind==n) return 0;
+        if(dp[ind][pre_ind+1]!=-1) return dp[ind][pre_ind+1];
+
+        int notTaken = 0 + getAns(nums,ind+1,pre_ind,n,dp);
+        int taken = 0;
+        if(pre_ind == -1 || nums[ind]>nums[pre_ind]){
+            taken = 1 + getAns(nums,ind+1,ind,n,dp);
         }
-        return maxi;
+        return dp[ind][pre_ind+1] = max(taken,notTaken);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n,vector<int>(n+1,-1));
+       
+        return getAns(nums,0,-1,n,dp);
     }
 };
