c607a66293da56d3c06a3797f3d6c517cf8f5a0c
SD691 <110734465+SD691@users.noreply.github.com>
2024-02-15 20:35:56 +0530

    Time: 62 ms (46.29%) | Memory: 21.4 MB (83.24%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -2,40 +2,35 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        if(grid[0][0]!=0) return -1;
-        if(grid[n-1][n-1]!=0) return -1;
-        queue<pair<pair<int,int>,int>> q;
-        vector<vector<int>> vis(n,vector<int>(n,-1));
-        q.push({{0,0},1});
-        int sum = INT_MAX;
+        int m = grid[0].size();
+        if(grid[n-1][m-1]!=0 || grid[0][0]!=0) return -1;
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
 
-        while(!q.empty()){
-            //auto it: q.front();
-            
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int level = q.front().second;
-            q.pop();
-            if(vis[row][col]==1) continue;
-            if(row==n-1 && col==n-1){
-                sum = min(sum,level);
+        pq.push({1,{0,0}});
+
+        int drow[] = {-1,-1,-1,0,1,1,1,0};
+        int dcol[] = {-1,0,1,1,1,0,-1,-1};
+        while(!pq.empty()){
+            auto it= pq.top();
+            pq.pop();
+            int level = it.first;
+            int row = it.second.first;
+            int col = it.second.second;
+
+            if(row==n-1 && col==m-1){
+                return level;
             }
-            vis[row][col] = 1;
-            int drow[] = {-1,-1,-1,0,1,1,1,0};
-            int dcol[] = {-1,0,1,1,1,0,-1,-1};
-
             for(int i=0; i<8; i++){
                 int nrow = row+drow[i];
                 int ncol = col+dcol[i];
-                if(nrow>-1 && nrow<n && ncol>-1 && ncol<n && grid[nrow][ncol]==0 && vis[nrow][ncol]==-1){
-                    q.push({{nrow,ncol},level+1});
-                }
-            } 
-        }
-        if(sum==INT_MAX){
-            return -1;
-        }
-        return sum;
 
+                if(nrow>-1 && nrow<n && ncol>-1 && ncol<m && grid[nrow][ncol]==0) {
+                    pq.push({level+1,{nrow,ncol}});
+                    grid[nrow][ncol] = 1;
+                }
+
+            }
+        }
+        return -1;
     }
 };
