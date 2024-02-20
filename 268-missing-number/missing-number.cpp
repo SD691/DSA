f2fc7eb1b0f1fc235c906fd8a3bdcf3f21ade448
SD691 <110734465+SD691@users.noreply.github.com>
2024-02-20 19:26:44 +0530

    Time: 7 ms (97.83%) | Memory: 21.1 MB (10.18%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int count = 0;
+        for(int i=0; i<nums.size(); i++){
+                if(nums[i]==count){
+                    count++;
+                }
+                else{
+                    return count;
+                }
+        }
+        return count;
+    }
+};
