1d258342164ed95712557d12c3334b7149f99206
SD691 <110734465+SD691@users.noreply.github.com>
2024-02-23 20:26:44 +0530

    Time: 11 ms (95.21%) | Memory: 16.5 MB (57.67%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -3,33 +3,40 @@ public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<pair<int,int>> adj[n];
         for(int i=0; i<flights.size(); i++){
-            adj[flights[i][0]].push_back({flights[i][1],flights[i][2]});
+            int edg = flights[i][0];
+            int adjE = flights[i][1];
+            int wt = flights[i][2];
+            adj[edg].push_back({adjE,wt});
         }
-        queue<pair<int,pair<int,int>>> pq;
-        pq.push({0,{src,0}});
-        vector<int> dis(n,1e9); 
 
-        while(!pq.empty()){
-            auto node = pq.front();
-            pq.pop();
-            int price = node.first;
-            int adjNode = node.second.first;
-            int level = node.second.second;
+        queue<pair<int,pair<int,int>>> q;
 
+        q.push({src,{0,0}});
+        vector<int> dis(n,1e9);
+        dis[src] = 0;
+        while(!q.empty()){
+            auto it= q.front();
+            q.pop();
+            int edg = it.first;
+            int wt = it.second.first;
+            int level = it.second.second;
 
-            for(auto it: adj[adjNode]){
-                int adjn = it.first;
-                int cost= it.second;
-                if(dis[adjn]>cost+price && level<=k){
-                    dis[adjn] = cost+price;
-                    pq.push({price+cost,{adjn,level+1}});
+            if(level==k+1){
+                continue;
+            }
+
+            for(auto it: adj[edg]){
+                int node = it.first;
+                int dist = it.second;
+                if(dis[node]>wt+dist){
+                    dis[node] = wt+dist;
+                    q.push({node,{dis[node],level+1}});
                 }
             }
-             
         }
-        if(dis[dst]!=1e9){
-            return dis[dst];
+        if(dis[dst]==1e9){
+            return -1;
         }
-        return -1;
+        return dis[dst];
     }
 };
