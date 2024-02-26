05ea976504b3b361efc97c22b12437bf0ff9f085
SD691 <110734465+SD691@users.noreply.github.com>
2024-02-27 01:09:32 +0530

    Time: 17 ms (43.04%) | Memory: 25.9 MB (69.93%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,30 +11,28 @@
  */
 class Solution {
 public:
+    TreeNode* build(vector<int> &preorder, int prestart, int preend, vector<int> &inorder, int instart, int inend, map<int,int> &mp){
+        if(prestart>preend || instart>inend) return NULL;
+        TreeNode* root = new TreeNode(preorder[prestart]);
 
-    TreeNode* constructTree(vector<int> &preorder, int preStart, int preEnd, vector<int> &inorder, int inStart, int inEnd, map<int,int> &mp){
-        if (preStart > preEnd || inStart > inEnd) return NULL;
-        TreeNode* root = new TreeNode(preorder[preStart]);
+        int ele = mp[root->val];
+        int nele = ele-instart;
 
-        int elem = mp[root->val];
-        int nElem = elem - inStart;
-
-        root->left = constructTree(preorder,preStart+1,preStart+nElem, inorder,inStart,elem-1,mp);
-        root->right = constructTree(preorder,preStart+nElem+1,preEnd,inorder,elem+1,inEnd,mp);
-
+        root->left = build(preorder,prestart+1,prestart+nele,inorder,instart,ele-1,mp);
+        root->right = build(preorder,prestart+nele+1,preend,inorder,ele+1,inend,mp);
         return root;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int preStart = 0;
-        int preEnd = preorder.size()-1;
-        int inStart = 0;
-        int inEnd = inorder.size()-1;
+        int prestart = 0;
+        int preend = preorder.size()-1;
+        int instart = 0;
+        int inend = inorder.size()-1;
 
         map<int,int> mp;
-        for(int i=inStart; i<=inEnd; i++){
+        for(int i=0; i<inorder.size(); i++){
             mp[inorder[i]] = i;
         }
 
-        return constructTree(preorder,preStart,preEnd,inorder,inStart,inEnd,mp);
+        return build(preorder,prestart,preend,inorder,instart,inend,mp);
     }
 };
