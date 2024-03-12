cbb049fc817b085c1dab1cb8ec40085666443fe2
SD691 <110734465+SD691@users.noreply.github.com>
2024-03-12 20:23:00 +0530

    Time: 98 ms (63.57%) | Memory: 69.9 MB (71.41%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        return sun(nums ,k) - sun(nums,k-1);
-        
+        return sun(nums,k)-sun(nums,k-1);
     }
-
-    int sun(vector<int>& nums, int sum){
-        int j = 0;
+    int sun(vector<int> &nums, int k){
+       
+        int j=0;
         int count = 0;
         int total = 0;
-
         for(int i=0; i<nums.size(); i++){
-            if(nums[i]%2 != 0){
+            if(nums[i]%2!=0){
                 count++;
             }
-
-            while(count>sum){
-                if(nums[j]%2 != 0){
+            
+            while(count>k){
+                if(nums[j]%2!=0){
                     count--;
-                    j++;
                 }
-                else{
-                    j++;
-                }
+                j++;
             }
-            total = total + (i-j+1);
+            total += i-j+1;
         }
         return total;
     }
