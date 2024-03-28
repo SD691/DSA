ab688fe799547dd9a53b75658f127b506f0580bc
SD691 <110734465+SD691@users.noreply.github.com>
2024-03-29 04:34:04 +0530

    Time: 3 ms (82.14%) | Memory: 16.4 MB (35.59%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-
-    void helper(int i, int target, vector<int> &cand, vector<int> &temp, vector<vector<int>> &ans){
-        if(i==cand.size()){
-            if(target == 0){
-                ans.push_back(temp);
-            }
+    void find(int ind, vector<vector<int>> &ans, vector<int> &temp, vector<int> &arr, int k){
+        if(ind==arr.size()){
+            if(k==0){
+            ans.push_back(temp);
+        }
             return;
         }
-
         
-        if(cand[i]<=target){
-            temp.push_back(cand[i]);
-            helper(i,target-cand[i],cand,temp,ans);
+
+        find(ind+1,ans,temp,arr,k);
+        if(arr[ind]<=k){
+            temp.push_back(arr[ind]);
+            find(ind,ans,temp,arr,k-arr[ind]);
             temp.pop_back();
         }
-        helper(i+1,target,cand,temp,ans);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> temp;
-        helper(0,target, candidates, temp, ans);
+        find(0,ans,temp,candidates,target);
         return ans;
     }
 };
