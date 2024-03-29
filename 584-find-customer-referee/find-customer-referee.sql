e8226b1668339be136b93d6b017952d7c2ae1cc6
SD691 <110734465+SD691@users.noreply.github.com>
2024-03-29 16:44:32 +0530

    Time: 1346 ms (9.74%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,3 +1,17 @@
 # Write your MySQL query statement below
 select name from customer
-where referee_id!=2 or referee_id = 1 or referee_id IS NULL;
+where referee_id != 2 or referee_id is NULL
+
+/*
+-- Checking for NULL values
+SELECT * FROM table_name WHERE column_name IS NULL;
+
+-- Checking for non-NULL values
+SELECT * FROM table_name WHERE column_name IS NOT NULL;
+
+-- Checking for TRUE values
+SELECT * FROM table_name WHERE boolean_column IS TRUE;
+
+-- Checking for FALSE values
+SELECT * FROM table_name WHERE boolean_column IS FALSE;
+*/
