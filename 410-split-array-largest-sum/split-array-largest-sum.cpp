86ac42c2d980a901b27ab7985564fb50d6c26b01
SD691 <110734465+SD691@users.noreply.github.com>
2024-03-29 19:53:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (36.17%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
-    int solve(vector<int> &nums , int mid){
-        int total = 0;
-        int student =1;
+    int bs(vector<int> &nums, int mid){
+        int total=0;
+        int student=  1;
         for(int i=0; i<nums.size(); i++){
-            if(total+nums[i]<=mid){
+            if(nums[i]+total<=mid){
                 total += nums[i];
             }
             else{
                 student++;
                 total = nums[i];
             }
-
         }
         return student;
     }
     int splitArray(vector<int>& nums, int k) {
-        if(nums.size()<k) return -1;
-        //sort(nums.begin(),nums.end());
-        int low = *max_element(nums.begin(),nums.end());
-        int high = accumulate(nums.begin(),nums.end(),0);
-        
-
+        int low = 0;
+        int high = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            low = max(low,nums[i]);
+            high += nums[i]; 
+        }
+        int ans = INT_MAX;
         while(low<=high){
             int mid = (low+high)/2;
-            int val = solve(nums,mid);
+            int val = bs(nums,mid);
 
-            if(val>k){
-                low=  mid+1;
+            if(val==k){
+                ans = min(ans,mid);
+                high = mid-1;
+            }
+            else if(val>k){
+                low = mid+1;
             }
             else{
                 high = mid-1;
             }
         }
         return low;
-
     }
 };
