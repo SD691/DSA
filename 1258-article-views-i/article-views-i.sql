0d31e5fec9a954dc9cc648e8fcb91d07c2dc203e
SD691 <110734465+SD691@users.noreply.github.com>
2024-03-29 16:50:48 +0530

    Time: 1428 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,3 +1,21 @@
-select distinct author_id as id from Views
-where author_id = viewer_id 
-order by id;
+# Write your MySQL query statement below
+select distinct author_id as id from views
+where author_id=viewer_id
+order by id
+
+/*-- Ascending order (default)
+SELECT column1, column2
+FROM table_name
+ORDER BY column1;
+
+-- Descending order
+SELECT column1, column2
+FROM table_name
+ORDER BY column1 DESC;
+
+-- Sorting by multiple columns
+SELECT column1, column2
+FROM table_name
+ORDER BY column1, column2;
+*/
+
