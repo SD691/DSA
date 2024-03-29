7e51d5f2fee703518df8d08ba50ecaeb2f24bcd5
SD691 <110734465+SD691@users.noreply.github.com>
2024-03-29 16:53:04 +0530

    Time: 1511 ms (14.78%) | Memory: 0B (100.00%) - LeetSync

--- 1827-invalid-tweets/invalid-tweets.sql
+++ 1827-invalid-tweets/invalid-tweets.sql
@@ -1,2 +1,25 @@
 # Write your MySQL query statement below
-select tweet_id from Tweets where length(content)>15;
+select tweet_id from tweets where length(content)>15
+
+/*
+To count the number of characters in a string column in SQL, 
+you can use the LENGTH() function in most SQL databases. 
+However, some databases like MySQL have CHAR_LENGTH() specifically 
+for counting characters. Here's how you can use these functions:
+
+Using LENGTH():
+
+sql
+Copy code
+SELECT LENGTH(column_name) AS character_count
+FROM table_name;
+Using CHAR_LENGTH() in MySQL:
+
+sql
+Copy code
+SELECT CHAR_LENGTH(column_name) AS character_count
+FROM table_name;
+Replace column_name with the name of the column you want to count characters from, and table_name with the name of your table.
+
+These queries will return the number of char
+*/
