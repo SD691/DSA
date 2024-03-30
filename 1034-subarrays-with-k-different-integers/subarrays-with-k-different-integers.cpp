2f4f8b4a701e79871d71687a554573d55e6979ed
SD691 <110734465+SD691@users.noreply.github.com>
2024-03-31 01:45:29 +0530

    Time: 113 ms (48.62%) | Memory: 49.8 MB (34.15%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
-    int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return (diff(nums,k) - diff(nums,k-1));
-    }
-
-    int diff(vector<int>& nums, int sum){
+    int diff(vector<int> &nums, int k){
         unordered_map<int,int> mp;
 
         int total = 0;
@@ -14,17 +10,21 @@ public:
 
         while(r<n){
             mp[nums[r]]++;
-            while(mp.size()>sum){
+            while(mp.size()>k){
                 mp[nums[l]]--;
                 if(mp[nums[l]]==0){
                     mp.erase(nums[l]);
                 }
                 l++;
             }
-            total = total + (r-l+1);
+            total = total+(r-l+1);
             r++;
 
         }
         return total;
     }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return diff(nums,k)-diff(nums,k-1);
+    }
+
 };
