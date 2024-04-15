ff118e86c5ee6607b122ea1baeb394cbb94572f9
SD691 <110734465+SD691@users.noreply.github.com>
2024-04-16 03:49:56 +0530

    Time: 881 ms (75.18%) | Memory: 0B (100.00%) - LeetSync

--- 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,3 +1,5 @@
 # Write your MySQL query statement below
-select teacher_id, count(DISTINCT subject_id) as cnt from teacher
-group by teacher_id
+select teacher_id, count(distinct(subject_id)) as cnt from teacher
+group by teacher_id 
+
+/*https://chat.openai.com/share/65bb69e3-da96-4c9e-a315-76c189579388*/
