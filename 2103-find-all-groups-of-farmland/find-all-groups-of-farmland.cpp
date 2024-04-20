58d19d29e542a8819885885acf6a8cebf89a06a3
SD691 <110734465+SD691@users.noreply.github.com>
2024-04-20 18:42:31 +0530

    Time: 142 ms (42.18%) | Memory: 141.8 MB (18.91%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -13,28 +13,15 @@ public:
             int col = q.front().second;
             q.pop();
 
-            int row1,row2,col1,col2;
-             row1 = row;
-             col1 = col+1;
-             row2 = row+1;
-             col2 = col;
-
-            bool val = false;
-            if(((col1<land[0].size() && land[row1][col1]==0) || col1==land[0].size()) && 
-            ((row2<land.size() && land[row2][col2]==0) || row2==land.size())){
-                val = true;
+            if(((col+1<land[0].size() && land[row][col+1]==0) || col+1==land[0].size()) && 
+            ((row+1<land.size() && land[row+1][col]==0) || row+1==land.size())){
+                p.first = row;
+                p.second = col;
+                break; 
             }
-            if(val==true){
-              p.first = row;
-              p.second = col;
-              break; 
-            }
-
             for(int i=0; i<4; i++){
                 int nrow = row+drow[i];
                 int ncol = col+dcol[i];
-
-                
                 if(nrow>=0 && nrow<land.size() && ncol>=0 && ncol<land[0].size() && 
                 land[nrow][ncol]==1 && vis[nrow][ncol]==-1){
                     q.push({nrow,ncol});
@@ -53,18 +40,11 @@ public:
         vector<vector<int>> ans;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                
                 if(land[i][j]==1 && vis[i][j]==-1){
-                    vector<int> temp;
-                    temp.push_back(i);
-                    temp.push_back(j);
                     vis[i][j]=1;
                     pair<int,int> pm = bfs(land,vis,i,j);
-                    temp.push_back(pm.first);
-                    temp.push_back(pm.second);
-                    ans.push_back(temp);
-                }
-                
+                    ans.push_back({i, j, pm.first, pm.second});
+                }   
             }
         }
         return ans;
