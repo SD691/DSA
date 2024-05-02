a781fd1d72ff9271d2019883d7f0240162c07be5
SD691 <110734465+SD691@users.noreply.github.com>
2024-05-02 19:09:23 +0530

    Time: 765 ms (36.48%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,9 +1,8 @@
-# Write your MySQL query statement below
-Select m.name
-from employee as e
-inner join employee as m
-on e.managerId=m.id
-group by e.managerId 
-having count(e.id)>=5
+select e1.name from employee e1
+inner join
+employee e2
+on e1.id=e2.managerId
+group by e1.id
+having count(e2.managerId)>4
 
-#https://chat.openai.com/share/8d8c5877-736c-4252-94a0-dbb399c68bd2
+
