3890795a9c59b0171b63063bfa11a56ba6752046
SD691 <110734465+SD691@users.noreply.github.com>
2024-05-04 23:07:27 +0530

    Time: 9 ms (62.09%) | Memory: 14.9 MB (96.35%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -10,25 +10,26 @@
  */
 class Solution {
 public:
-    void reverse(ListNode* root){
-        ListNode* curr = root;
-        ListNode* next = NULL;
-        ListNode* prev= NULL;
 
-        while(curr){
+    void reverse(ListNode* s, ListNode* e){
+        ListNode* curr = s;
+        ListNode* next = NULL;
+        ListNode* prev = NULL;
+
+        while(curr!=NULL){
             next = curr->next;
             curr->next = prev;
             prev = curr;
             curr = next;
         }
-        
     }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(head==NULL || head->next==NULL || k==1){
+        if(head==NULL || head->next==NULL && k==1){
             return head;
         }
-        ListNode* s = head;
-        ListNode* e = head;
+
+        ListNode* s=head;
+        ListNode* e=head;
         int val = k-1;
         while(val){
             if(e->next==NULL){
@@ -38,11 +39,10 @@ public:
             val--;
         }
 
-        ListNode* next_node = reverseKGroup(e->next,k);
-        e->next = NULL;
-        reverse(s);
-        s->next = next_node;
+        ListNode* root = reverseKGroup(e->next,k);
+        e->next=NULL;
+        reverse(s,e);
+        s->next = root;
         return e;
-
     }
 };
