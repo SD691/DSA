bc6f1f6b842ba20ad32c8b6919b6103545438d7f
SD691 <110734465+SD691@users.noreply.github.com>
2024-05-27 23:44:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (74.49%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
 
-    void helper(string &digits, int start, string &temp, vector<string> &ans, map<int,string> mp){
-        if(start == digits.size()){
-            ans.push_back(temp);
+    void solve(int ind, string s, vector<string> &ans,unordered_map<char,string> &mp, string &temp){
+        if(ind==s.size()){
+            if(temp.size()>0){
+                ans.push_back(temp);
+            }
+            
             return;
         }
 
-        for(auto it: mp[digits[start]-'0']){
+        for(auto it:mp[s[ind]]){
             temp.push_back(it);
-            helper(digits,start+1,temp,ans,mp);
+            solve(ind+1,s,ans,mp,temp);
             temp.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
-        if(digits.size()==0){
-            return {};
-        }
-        map<int,string> mp;
+        unordered_map<char,string> mp;
+        mp['2'] = "abc";
+        mp['3'] = "def";
+        mp['4'] = "ghi";
+        mp['5'] = "jkl";
+        mp['6'] = "mno";
+        mp['7'] = "pqrs";
+        mp['8'] = "tuv";
+        mp['9'] = "wxyz";
 
-        mp[2] = "abc";
-        mp[3] = "def";
-        mp[4] = "ghi";
-        mp[5] = "jkl";
-        mp[6] = "mno";
-        mp[7] = "pqrs";
-        mp[8] = "tuv";
-        mp[9] = "wxyz";
-
-        string temp;
         vector<string> ans;
-        helper(digits,0,temp,ans,mp);
+        string temp;
+        solve(0,digits,ans,mp,temp);
+        
         return ans;
     }
 };
