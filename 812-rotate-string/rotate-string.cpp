f9f436d735bafc742e04483871d7d5d028005685
SD691 <110734465+SD691@users.noreply.github.com>
2024-06-02 01:17:29 +0530

    Time: 3 ms (48.33%) | Memory: 7.3 MB (62.94%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,28 +1,11 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-       if (s.size() != goal.size()) return false;
-
-        string to = s + s;
-
-        for (int i = 0; i < s.size(); i++) {
-            int j = 0;
-            vector<char> ans;
-
-            while (j < goal.size() && to[i] == goal[j]) {
-                ans.push_back(to[i]);
-                i++;
-                j++;
-            }
-
-            if (j == goal.size()) {
-                return true;
-            }
-
-            // Reset i to the beginning of s, consider the next possible rotation
-            i -= ans.size();
+        if(goal.size()!=s.size()){
+            return false;
         }
-
-        return false;
+        s = s+s;
+        bool val = s.find(goal)!=string::npos;
+        return val;
     }
 };
