dde7412871c185ee6c967804d4ba9007569951c4
SD691 <110734465+SD691@users.noreply.github.com>
2025-10-02 05:03:21 +0530

    Time: 19 ms (82.34%) | Memory: 18 MB (73.22%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    int helper(int ind, vector<int> &coins, int amount, vector<vector<int>> &dp){
-        
-        if(ind==0){
-            if(amount%coins[0]==0) return amount/coins[0];
-            else return 1e9;
-        }
-        if(amount==0) return 0;
-        if(dp[ind][amount]!=-1) return dp[ind][amount];
-        int notTaken = helper(ind-1,coins,amount,dp);
-        int taken = 1e9;
-        if(coins[ind]<=amount){
-            amount= amount-coins[ind];
-            taken = 1+helper(ind,coins,amount,dp);
-            amount = amount+coins[ind];
-        }
-        return dp[ind][amount] = min(taken,notTaken);
+    int coinChange(vector<int>& arr, int x) {
+        // int x = amount;
+        int n = arr.size();
 
+        vector<int> dp(x+1,1e9);
+        dp[0]=0;
+    
+        for(int i=1; i<=x; i++){
+            for(int j=0; j<n; j++){
+                if(arr[j]<=i){
+                    dp[i]= min(dp[i],dp[i-arr[j]]+1);
+                }
+            }
+        }
+    
+        if(dp[x]==1e9){
+            return -1;
+        }
+        return dp[x];
     }
-    int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
-        int val = helper(n-1,coins,amount,dp);
-        if(val==1e9) return -1;
-        return val;
-    }
+    
 };
