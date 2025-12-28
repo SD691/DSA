c8c085f5dabefb4c8878a9b5a9f0cc33ac68433e
SD691 <110734465+SD691@users.noreply.github.com>
2025-12-28 23:02:43 +0530

    Time: 59 ms (6.80%) | Memory: 54.6 MB (71.06%) - LeetSync

---
+++ 2864-is-object-empty/is-object-empty.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    // if(JSON.stringify(obj).length<=2) return true;
+    // else return false;
+
+    // return Object.keys(obj).length===0
+
+    for(const a in obj) return false;
+    return true;
+};
