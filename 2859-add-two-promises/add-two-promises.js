ba3aa225a602856812a9d54d291f0224fa101e7f
SD691 <110734465+SD691@users.noreply.github.com>
2025-12-31 02:14:33 +0530

    Time: 54 ms (62.99%) | Memory: 54.8 MB (45.86%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -6,14 +6,14 @@
 var addTwoPromises = async function(promise1, promise2) {
     // const p1 = promise1;
     // const p2 = promise2;
-    const [result1, result2] = await Promise.all([promise1,promise2]);
-    return result1+result2;
-    // let sum = 0;
-    // var addTwo = async function func(promise1, promise2){
-    //     const [result1, result2] = await Promise.all([promise1,promise2]);
-    //     sum = result1+result2;
-    // }
-    // return sum;
+    // const [result1, result2] = await Promise.all([promise1,promise2]);
+    // return result1+result2;
+    let sum = 0;
+    var addTwo = async function func(promise1, promise2){
+        const [result1, result2] = await Promise.all([promise1,promise2]);
+        return result1+result2;
+    }
+    return sum = await addTwo(promise1,promise2);
     // Promise.all(
     //     [
     //         promise1,
