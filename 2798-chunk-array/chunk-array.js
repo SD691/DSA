541f7309397a42edc8bd53500f89e875e464f63b
SD691 <110734465+SD691@users.noreply.github.com>
2026-01-12 03:41:19 +0530

    Time: 52 ms (39.12%) | Memory: 56.9 MB (26.39%) - LeetSync

--- 2798-chunk-array/chunk-array.js
+++ 2798-chunk-array/chunk-array.js
@@ -4,24 +4,24 @@
  * @return {Array}
  */
 var chunk = function(arr, size) {
-    
-    let ans = [];
-    let count=0;
+    let answer= [];
+    let count = 0;
     let temp = [];
     for(let i=0; i<arr.length; i++){
-        if(count==size){
-            count=0;
-            ans.push(temp);
-            temp = [];
-        }
         if(count<size){
             count++;
             temp.push(arr[i]);
         }
-    }
+        else{
+            count=1;
+            answer.push(temp);
+            temp=[];
+            temp.push(arr[i]);
 
-    if(temp.length){
-        ans.push(temp);
+        }
     }
-    return ans;
+    if(count>0){
+        answer.push(temp);
+    }
+    return answer;
 };
