91cbd19772ad8b6a50c48c87b75a0fc0b69b7151
SD691 <110734465+SD691@users.noreply.github.com>
2026-01-14 04:14:55 +0530

    Time: 52 ms (19.64%) | Memory: 55.1 MB (6.67%) - LeetSync

---
+++ 2796-allow-one-function-call/allow-one-function-call.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let result = [{"calls":1,"value":0}];
+    let count=true;
+    return function(...args){
+        if(count){
+            count = false;
+            return fn(...args);
+            
+        }
+    }
+    
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
